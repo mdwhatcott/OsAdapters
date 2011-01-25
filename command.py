4a52959c90d29f72d1dd7d1461a3873f0b3d1dc2
Whatcott, Michael <mwhatcott@localhost>
2011-01-25 15:03:04 -0700

    Refined the command object.

--- command.py
+++ command.py
@@ -1,12 +1,17 @@
 import os
+import string
 import contextlib
 import subprocess
 
+_SPACE = string.whitespace[-1]
+_BLANK = str()
 
 class Command(object):
+    NO_OP = '(...no op...)\n{0}>{1}'
+
     def __init__(self, text=None):
         self._command = [text] if text is not None else []
-        self._working_location = '.'
+        self._working_location = os.getcwd() 
         self._no_op = False
         self._verbose = False
 
@@ -45,9 +50,8 @@ class Command(object):
             return subprocess.Popen(self._command)
 
     def _no_op_report(self):
-        print 'FROM:', self._working_location
-        print 'COMMAND:', ' '.join(self._command)
-        return ''
+        print Command.NO_OP.format(self._working_location, _SPACE.join(self._command))
+        return _BLANK
 
     @contextlib.contextmanager
     def _working_context(self, location):

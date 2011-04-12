885b56a06f3dbb0e50bf90b662193ab012b65100
Whatcott, Michael <mwhatcott@localhost>
2011-04-12 16:34:11 -0600

    Added a Location class.

--- file_system.py
+++ file_system.py
@@ -53,3 +53,56 @@ class FileSystem(object):
 
     def copy(self, src, dest):
         shutil.copy(src, dest)
+
+
+class Location(object):
+    def __init__(self, path):
+        if not os.path.exists(path):
+            raise NameError("Path does not exist: {0}".format(path))
+
+        self.path = os.path.abspath(path)
+
+    @property
+    def isfile(self):
+        return os.path.isfile(self.path)
+
+    @property
+    def isdir(self):
+        return os.path.isdir(self.path)
+
+    @property
+    def filename(self, with_extension=True):
+        if not self.isfile:
+            return ''
+
+        name = self.components[-1]
+
+        if not with_extension and '.' in name:
+            return '.'.join(name.split('.')[:-1])
+
+        return name
+
+    @property
+    def extension(self):
+        return '' if not self.isfile else self.basename.split('.')[-1]
+
+    @property
+    def components(self):
+        return self.path.split(os.path.sep)
+
+    @property
+    def children(self):
+        return [Location(os.path.join(self.path, child)) for child in os.listdir(self.path)]
+
+    @property
+    def parent(self):
+        if os.path.isfile(self.path):
+            return Location(os.path.dirname(self.path))
+
+        return Location(os.path.abspath(os.path.join(self.path, os.path.pardir)))
+
+    def __str__(self):
+        return self.path
+
+    def __repr__(self):
+        return "Location('{0}')".format(self.path)

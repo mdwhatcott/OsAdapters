19d1a9f89f598aab6d25afafae4a8f2162f56ee9
Whatcott, Michael <mdwhatcott@gmail.com>
2011-07-04 07:40:25 -0600

    Bundled as a package on PyPI.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='OsAdapters',
+    author='Michael Whatcott',
+    author_email='mdwhatcott@gmail.com',
+    url='https://www.github.com/mdwhatcott/OsAdapters',
+    version='0.1',
+    license='The MIT License (MIT)',
+    description='Nice abstractions over the file system and running commands.',
+    long_description=open('README').read(),
+    packages=['iterable'],
+)
